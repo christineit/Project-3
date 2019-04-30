46d0e0ccf92ba04edd9587a11bee5e3c24b1925b
Christine <christineitrinh@gmail.com>
2019-04-30 16:11:24 -0500

    changed Navbar anchor text

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <a className="navbar-brand" href="/">
-        React Reading List
+        Home
       </a>
     </nav>
   );
