f0a6da38d869b233e0f51cb722b4fd372fa48add
Golden Dragon <arajchwald@gmail.com>
2019-05-01 13:43:56 -0500

    fake admin and soon to be form

---
+++ client/src/components/AdminStuff/ProdForm.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+export default class ProdForm extends React.Component {
+
+    state = {
+        product: "",
+
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,11 +1,12 @@
 import React from "react";
-
+import AdminForm from "../components/AdminStuff/AdminForm"
 export default class Admin extends React.Component {
     render() {
         return (
             <div>
                 Nav Working Admin
-             </div>
+                <AdminForm />
+            </div>
         )
     }
 }
