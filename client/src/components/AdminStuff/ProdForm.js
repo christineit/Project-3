2bba7f293d360642d0218917e71e91d11fc381b0
Golden Dragon <arajchwald@gmail.com>
2019-05-01 14:44:16 -0500

    admin stuff, forms, submits etc

--- client/src/components/AdminStuff/ProdForm.js
+++ client/src/components/AdminStuff/ProdForm.js
@@ -1,16 +1,48 @@
 import React from "react"
-
-export default class ProdForm extends React.Component {
+import { Jumbotron, Container } from 'reactstrap';
+import CreateProductForm from './CreateProductForm'
+export default class ProductCreate extends React.Component {
 
     state = {
-        product: "",
+        productname: "",
+        category: "",
+        size: "",
+        quantity: 0,
+        description: "",
+        price: 0,
+        images: "",
+        keywords: ""
 
+    }
+
+    handleInputChange = e => {
+        let value = e.target.value;
+        let yeet = e.target.name;
+
+        this.setState({
+            [yeet]: value
+        })
+    }
+
+    handleFormSubmit = e => {
+        e.preventDefault();
+        console.log(this.state)
     }
 
     render() {
         return (
             <div>
+                <Jumbotron fluid>
+                    <Container fluid>
+                        <h1 className="display-3">Sell A Product</h1>
+                        <p className="lead">Specify product details below, then submit product to site</p>
+                        <CreateProductForm
+                            handleFormSubmit={this.handleFormSubmit}
+                            handleInputChange={this.handleInputChange}
 
+                        />
+                    </Container>
+                </Jumbotron>
             </div>
         )
     }
--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,11 +1,13 @@
 import React from "react";
 import AdminForm from "../components/AdminStuff/AdminForm"
+import ProdForm from "../components/AdminStuff/ProdForm"
 export default class Admin extends React.Component {
     render() {
         return (
             <div>
                 Nav Working Admin
                 <AdminForm />
+                <ProdForm />
             </div>
         )
     }
