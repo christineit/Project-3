2551c7b0a2df75b713721030ccb1d63c4e6c68bb
Christine <christineitrinh@gmail.com>
2019-05-02 12:10:06 -0500

    fixed firebase connection

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -3,11 +3,6 @@ import SomeComponent from "../components/Account";
 
 export default class Cart extends React.Component {
   render() {
-    return (
-      <div>
-        Nav Working CART
-        <SomeComponent />
-      </div>
-    );
+    return <div>Nav Working CART</div>;
   }
 }
