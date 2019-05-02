2df25f2ef97f2dafd244c712e7e1c6fe525a830c
chrislewisjones <47401704+chrislewisjones@users.noreply.github.com>
2019-05-02 11:32:36 -0500

    adding card

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 // import Container from "../components/Container";
 import { Container, Row, Col } from "reactstrap";
 import Carousel from "../components/Carousel";
-import Button from "../components/Button";
+import Card from "../components/Card";
 
 export default class Home extends React.Component {
   render() {
@@ -12,9 +12,9 @@ export default class Home extends React.Component {
           <Col sm="12" md={{ size: 6, offset: 3 }}>
             <h1>Home Page</h1>
             <Carousel />
-            <Button />
           </Col>
         </Row>
+        <Card />
       </Container>
     );
   }
