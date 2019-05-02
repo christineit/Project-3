0cfefdeda97b1b31af30d581178540e968f1fc9b
chrislewisjones <47401704+chrislewisjones@users.noreply.github.com>
2019-05-02 10:55:16 -0500

    added buy button

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 // import Container from "../components/Container";
 import { Container, Row, Col } from "reactstrap";
 import Carousel from "../components/Carousel";
+import Button from "../components/Button";
 
 export default class Home extends React.Component {
   render() {
@@ -11,6 +12,7 @@ export default class Home extends React.Component {
           <Col sm="12" md={{ size: 6, offset: 3 }}>
             <h1>Home Page</h1>
             <Carousel />
+            <Button />
           </Col>
         </Row>
       </Container>
