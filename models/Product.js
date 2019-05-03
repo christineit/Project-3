f2e75a842381110e322e842f79fbbd87f87268e0
Golden Dragon <arajchwald@gmail.com>
2019-05-03 10:55:29 -0500

    yeey

--- models/Product.js
+++ models/Product.js
@@ -2,16 +2,16 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
-  
+
   Name: { type: String, required: true },
-  Category: { type: String},
-  Size: { type: Array},
-  Quantity: {type: Number },
-  Description: { type: String},
-  Date: { type:Date, default: Date.now },
-  Price: {type:Number, required: true },
-  Images_URL:{type:Array},
-  Association:{type:Array}
+  Category: { type: String },
+  Size: { type: String },
+  Quantity: { type: Number },
+  Description: { type: String },
+  Date: { type: Date, default: Date.now },
+  Price: { type: Number, required: true },
+  Images_URL: { type: Array },
+  Association: { type: Array }
 });
 
 const Product = mongoose.model("Product", productSchema);
