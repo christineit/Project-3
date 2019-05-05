fbaa61c28c2c3297c12f1fa9e3a674766c798233
chrislewisjones <47401704+chrislewisjones@users.noreply.github.com>
2019-05-05 16:18:40 -0500

    update

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -2,12 +2,9 @@ import React from "react";
 import AdminForm from "../components/AdminStuff/AdminForm";
 import ProdForm from "../components/AdminStuff/ProdForm";
 import SomeComponent from "../components/Account";
-<<<<<<< HEAD
-import ProductPool from "../components/AdminStuff/ProductPool"
-=======
+import ProductPool from "../components/AdminStuff/ProductPool";
 import { FirebaseContext } from "../components/Firebase";
 
->>>>>>> master
 export default class Admin extends React.Component {
   render() {
     return (
