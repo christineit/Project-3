5bf83c3f2b763f1fd2b5c7e8df919f4c7f9a5f7c
Scott wittenberg <wittenb48@gmail.com>
2019-05-06 12:49:38 -0500

    Cart and Checkout

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import Firebase, { FirebaseContext } from "./components/Firebase";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import Client from "shopify-buy";
 
+import "./app.css";
 const client = Client.buildClient({
   storefrontAccessToken: "dd4d4dc146542ba7763305d71d1b3d38",
   domain: "graphql.myshopify.com"
