fb54c5d2ee74e180cc013e13287759f6e8507db6
chrislewisjones <47401704+chrislewisjones@users.noreply.github.com>
2019-05-06 21:33:12 -0500

    update

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Admin from "./pages/Admin";
 import Cart from "./components/shopify/Cart";
 import CartPage from "./pages/Cartpage.js";
 import Client from "shopify-buy";
+import Particles from "react-particles-js";
 
 function App(props) {
   const client = Client.buildClient({
@@ -16,25 +17,81 @@ function App(props) {
     domain: "graphql.myshopify.com"
   });
   return (
-    <Router>
-      <div>
-        <NavBar />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route
-            exact
-            path="/cart"
-            render={() => <CartPage client={props.client} />}
-          />
-          <Route exact path="/products" component={Products} />
-          <Route exact path="/admin" component={Admin} />
-          <Route path="/shop" component={() => <Shop client={client} />} />
-          {/* <Route exact path="/books" component={Books} />
-            <Route exact path="/books/:id" component={Detail} />
-            <Route component={NoMatch} /> */}
-        </Switch>
-      </div>
-    </Router>
+    <div>
+      <Router>
+        <div>
+          <NavBar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route
+              exact
+              path="/cart"
+              render={() => <CartPage client={props.client} />}
+            />
+            <Route exact path="/products" component={Products} />
+            <Route exact path="/admin" component={Admin} />
+            <Route path="/shop" component={() => <Shop client={client} />} />
+            {/* <Route exact path="/books" component={Books} />
+              <Route exact path="/books/:id" component={Detail} />
+              <Route component={NoMatch} /> */}
+          </Switch>
+        </div>
+      </Router>
+      <Particles
+        params={{
+          particles: {
+            number: {
+              value: 160,
+
+              density: {
+                enable: false
+              }
+            },
+            size: {
+              value: 3,
+              random: true,
+              anim: {
+                speed: 4,
+                size_min: 0.3
+              }
+            },
+            line_linked: {
+              enable: false
+            },
+            move: {
+              random: true,
+              speed: 1,
+              direction: "top",
+              out_mode: "out"
+            }
+          },
+          interactivity: {
+            events: {
+              onhover: {
+                enable: true,
+                mode: "bubble"
+              },
+              onclick: {
+                enable: true,
+                mode: "repulse"
+              }
+            },
+            modes: {
+              bubble: {
+                distance: 250,
+                duration: 2,
+                size: 0,
+                opacity: 0
+              },
+              repulse: {
+                distance: 400,
+                duration: 4
+              }
+            }
+          }
+        }}
+      />
+    </div>
   );
 }
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -11,15 +11,17 @@ import "./Home.css";
 export default class Home extends React.Component {
   render() {
     return (
-      <Container>
-        <Row>
-          <Col sm="12" md={{ size: 6, offset: 3 }}>
-            <h1>Home Page</h1>
-            <Carousel style={{ padding: "50px" }} />
-            <Card />
-          </Col>
-        </Row>
-      </Container>
+      <div className="background">
+        <Container>
+          <Row>
+            <Col sm="12" md={{ size: 6, offset: 3 }}>
+              <h1>Home Page</h1>
+              <Carousel style={{ padding: "50px" }} />
+              <Card />
+            </Col>
+          </Row>
+        </Container>
+      </div>
     );
   }
 }
