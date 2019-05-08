f15487d9d48cfb0ce978291f9955df784778a7fa
Peter Stone <peter.stone6391@gmail.com>
2019-05-08 10:28:41 -0500

    please work

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -8,17 +8,11 @@ import "./Home.css";
 import ProductYeet from "../components/ProductDisplay/ProductYeet";
 import CartPage from "./Cartpage";
 import Shop from "../shop";
-<<<<<<< HEAD
-import Footer from "../components/Footer/index";
-// export default class Home extends React.Component {
-import storeImage from "../Images/arms.jpg";
-=======
-import Footer from "../components/Footer"
+import Footer from "../components/Footer";
 import CustomCart from "./customcart.js";
 
 // export default class Home extends React.Component {
-import storeImage from "../images/arms.jpg";
->>>>>>> be625436f83e9eef72cafb49b7b63b0f32dac6d7
+
 const homeStyle = {
   backgroundImage: `url(${storeImage})`
 };
@@ -32,12 +26,6 @@ function Home(props) {
           <Col sm="12" md={{ size: 6, offset: 3 }}>
             <h1>Home Page</h1>
             <Carousel style={{ padding: "50px" }} />
-<<<<<<< HEAD
-          </Col>
-          <Col>
-            <CartPage client={props.client} />
-=======
->>>>>>> be625436f83e9eef72cafb49b7b63b0f32dac6d7
           </Col>
           <Col>{/* <CartPage client={props.client} /> */}</Col>
         </Row>
@@ -50,12 +38,8 @@ function Home(props) {
       <br />
       <br />
 
-<<<<<<< HEAD
-      <Footer />
-=======
       <CustomCart />
       {/* <Footer /> */}
->>>>>>> be625436f83e9eef72cafb49b7b63b0f32dac6d7
     </div>
   );
   // }
