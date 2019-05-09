19a8f1c33f923ec3fd769b3f52a45a2bd2b1dfbd
irfanp90 <irfanp90@gmail.com>
2019-05-09 13:51:38 -0500

    update

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -27,10 +27,10 @@ function Home(props) {
             <br />
             <br />
             <br />
-            <Carousel style={{ padding: "50px" }} />
+            {/* <Carousel style={{ padding: "50px" }} /> */}
           </Col>
           <Col sm="12" md={{ size: 6, offset: 3 }}>
-            <Card style={{ padding: "50px" }} />
+            {/* <Card style={{ padding: "50px" }} /> */}
           </Col>
 
           <Col>{/* <CartPage client={props.client} /> */}</Col>
